88549bb31b29811fa71ad7edc2e83e067f88edbb
RajabovIlyas <ilyasrajabow@gmail.com>
2023-12-15 14:22:19 +0500

    added dark mode

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -21,8 +21,8 @@ const Modal:FC<ModalProps> = ({showModal, saveId, setShowModal, onSave, children
           <div
             className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
             <div
-              className='flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t'>
-              <h3 className='text-3xl font-semibold'>
+              className='dark:bg-neutral-800 dark:border-neutral-700 flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t'>
+              <h3 className='dark:text-zinc-300 text-3xl font-semibold'>
                 Settings
               </h3>
               <button
@@ -30,24 +30,24 @@ const Modal:FC<ModalProps> = ({showModal, saveId, setShowModal, onSave, children
                 onClick={onClose}
               >
                     <span
-                      className='button-img bg-transparent text-black  text-2xl block '>
+                      className='dark:text-zinc-300 button-img bg-transparent text-black  text-2xl block '>
                       ×
                     </span>
               </button>
             </div>
-            <div className='relative p-6 flex-auto'>
+            <div className='relative p-6 flex-auto dark:bg-neutral-800'>
             {children}
             </div>
-            <div className='flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b'>
+            <div className='dark:bg-neutral-800 dark:border-neutral-700 flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b'>
               <button
-                className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                className='dark:text-red-700 text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                 type='button'
                 onClick={onClose}
               >
                 Close
               </button>
               <button
-                className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+                className='dark:bg-emerald-700 bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
                 type="submit"
                 form={saveId}
                 onClick={onSave}
@@ -58,7 +58,6 @@ const Modal:FC<ModalProps> = ({showModal, saveId, setShowModal, onSave, children
           </div>
         </div>
       </div>
-      <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
     </>
   )
 }
